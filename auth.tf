d5ede979a8040b6b4c85f48cf04450f4e690cac5
Helge Winkler <helge.winkler@hermesworld.com>
2021-03-08 14:14:17 +0100

    try something with auth map

--- auth.tf
+++ auth.tf
@@ -82,6 +82,8 @@ provider "kubernetes" {
   token                  = join("", data.aws_eks_cluster_auth.eks.*.token)
   host                   = join("", data.aws_eks_cluster.eks.*.endpoint)
   cluster_ca_certificate = base64decode(join("", data.aws_eks_cluster.eks.*.certificate_authority.0.data))
+
+  depends_on = [aws_eks_cluster.default]
 }
 
 resource "kubernetes_config_map" "aws_auth_ignore_changes" {
